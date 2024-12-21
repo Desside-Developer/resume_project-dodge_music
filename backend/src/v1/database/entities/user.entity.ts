d7103f9302dd91a366b95265cb6d547cf780dec8
Desside-Developer <desside.programming@gmail.com>
2024-12-21 08:13:28 +0200

    🔧 entity(backend): Add OneToMany tracks > Music.Entity

--- backend/src/v1/database/entities/user.entity.ts
+++ backend/src/v1/database/entities/user.entity.ts
@@ -1,6 +1,7 @@
 // filepath: /c:/Users/HCU-2248-562/Documents/Closed Projects/resume_project-dodge_music/backend/src/v1/database/entities/user.entity.ts
-import { Entity, Column, PrimaryGeneratedColumn, BeforeInsert, Unique } from 'typeorm';
-import * as bcrypt from 'bcryptjs';
+import { Entity, Column, PrimaryGeneratedColumn, BeforeInsert, Unique, OneToMany } from 'typeorm';
+import { Music } from './music.entity';
+// import * as bcrypt from 'bcryptjs';
 
 @Entity()
 @Unique(['email'])
@@ -12,8 +13,11 @@ export class User {
   name: string;
 
   @Column()
-  email: string;
+  email: string;  
 
   @Column()
   pass: string;
+
+  @OneToMany(() => Music, music => music.user)
+  tracks: Music[];
 }
